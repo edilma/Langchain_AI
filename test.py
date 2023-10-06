39537685aa4c8700e8307c285c41831f27d2f764
Edilma Riano <edilmaria@hotmail.com>
2023-10-06 14:14:13 -0400

    worked with LC schema

---
+++ test.py
@@ -0,0 +1,13 @@
+topic_list = [
+    "nasa", "local government", "engineering", 
+    "employee satisfaction", "federal government"
+]
+
+
+
+
+topic_dict = {i.split(': ')[0]: int(i.split(': ')[1]) for i in response.split(sep='\n')}
+
+topic_dict = 
+if topic_dict['nasa'] == 1:
+    print("ALERT: New NASA story!")
