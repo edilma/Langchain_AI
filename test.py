33126faaa143021951100bb525b085d2318e3ff8
Edilma Riano <edilmaria@hotmail.com>
2023-10-14 16:20:30 -0400

    WORKED

--- test.py
+++ test.py
@@ -9,5 +9,6 @@ topic_list = [
 topic_dict = {i.split(': ')[0]: int(i.split(': ')[1]) for i in response.split(sep='\n')}
 
 topic_dict = 
+
 if topic_dict['nasa'] == 1:
     print("ALERT: New NASA story!")
